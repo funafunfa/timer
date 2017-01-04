fa92af3134d01c091492723852bb78c547e7fc5d
funafunfa <golovolomka132@gmail.com>
2017-01-04 18:25:56 +0200

    assets

--- src/timer_main.java
+++ src/timer_main.java
@@ -1,9 +1,13 @@
+import javax.imageio.ImageIO;
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.TimerTask;
 
@@ -12,6 +16,7 @@ public class timer_main extends JFrame {
     int i, z, stop;
     JButton button1;
     Timer timer;
+    Clip clip;
     public timer_main(int stop) {
         super("Timer");
         this.stop = stop;
@@ -58,6 +63,12 @@ public class timer_main extends JFrame {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 timer_main frame = new timer_main(60);
+                try {
+                    frame.setIconImage(ImageIO.read(new File("img/timer-icon-7786.png")));
+                }
+                catch (IOException exc) {
+                    exc.printStackTrace();
+                }
                 frame.pack();
                 frame.requestFocus();
                 frame.setResizable(false);
@@ -73,14 +84,30 @@ public class timer_main extends JFrame {
         public void actionPerformed(ActionEvent e) {
             z++;
             Color WHITE = new Color(255,255,255);
-            if (z / 2 == 0){
+            if (z / 2 == 0) {
                 button1.setBackground(WHITE);
-                timer.start();
+                try {
+                    {
+                        String soundName = "img/181148__keweldog__timer-with-ding.wav";
+                        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundName).getAbsoluteFile());
+                        clip = AudioSystem.getClip();
+                        clip.open(audioInputStream);
+                        clip.start();
+                    }
+                    timer.start();
+                } catch (LineUnavailableException e1) {
+                    e1.printStackTrace();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                } catch (UnsupportedAudioFileException e1) {
+                    e1.printStackTrace();
+                }
             }
             else{
                 timer.stop();
                 z = 0;
                 i = 0;
+                clip.stop();
                 button1.setText("");
             }
         }
